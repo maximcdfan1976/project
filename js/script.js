a195ba84bfd8398f687ce02d5d72ded8d20f79ae
harmongocia <maximmikhaylov20101976@gmail.com>
2022-04-20 13:33:15 +0300

    Firts program

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 "use strict"
 
-console.log('arr' + " - object ");
+const numberOfFilms = prompt('How many movies you have seen?')
+
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: {},
+        privat: false
+};
+
+const a = prompt('One of your lost movies?',''),
+      b = prompt('Rate this movie?', ''),
+      c = prompt('One of your lost movies?',''),
+      d = prompt('Rate this movie?','');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
+
+
